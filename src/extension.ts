6ee0322ccb7cffcb7b6dd0dfb34a509075e8b833
Johan Rouve <johan.rouve@comet.team>
2024-05-06 11:34:06 +0200

    Improve suggestions

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,8 @@
+import * as O from "fp-ts/Option";
+import * as Rec from "fp-ts/Record";
 import * as vscode from "vscode";
 
 import * as A from "fp-ts/Array";
-import * as R from "fp-ts/Record";
 import { pipe } from "fp-ts/function";
 
 const dict = {
@@ -37,54 +38,69 @@ const lowercaseModuleNames = [
   "struct",
 ];
 export function activate(context: vscode.ExtensionContext) {
-  const importFormat = vscode.workspace
-    .getConfiguration("fp-ts-import")
-    .get("importFormat");
-  const completion = vscode.languages.registerCompletionItemProvider(
+  const importFormat = pipe(
+    O.fromNullable(
+      vscode.workspace
+        .getConfiguration("fp-ts-import")
+        .get<string>("importFormat")
+    ),
+    O.getOrElse(() => "")
+  );
+
+  const output = vscode.window.createOutputChannel("fp-ts-import", {
+    log: true,
+  });
+  output.debug("configuration", { importFormat });
+
+  const disposable = vscode.languages.registerCompletionItemProvider(
     ["typescript", "typescriptreact", "javascript", "javascriptreact"],
     {
-      provideCompletionItems(
-        document: vscode.TextDocument,
-        position: vscode.Position,
-        token: vscode.CancellationToken,
-        context: vscode.CompletionContext
-      ) {
-        const addCompletion = (alias: keyof typeof dict) => {
-          const rootImportName = dict[alias];
-          const completion = new vscode.CompletionItem(alias);
-          completion.additionalTextEdits = [
-            new vscode.TextEdit(
-              new vscode.Range(
-                new vscode.Position(0, 0),
-                new vscode.Position(0, 0)
-              ),
-              importFormat === "module"
-                ? `import * as ${alias} from "fp-ts/${
-                    lowercaseModuleNames.includes(rootImportName)
-                      ? rootImportName
-                      : capitalize(rootImportName)
-                  }";\n`
-                : `import { ${rootImportName} as ${alias} } from "fp-ts";\n`
-            ),
-          ];
-          completion.kind = vscode.CompletionItemKind.Module;
-          completion.commitCharacters = ["."];
-          completion.insertText = alias;
-          completion.command = {
-            command: "editor.action.triggerSuggest",
-            title: "Re-trigger completions...",
-          };
+      provideCompletionItems(document, position, ___, context) {
+        const previousCharacter = document.getText(
+          new vscode.Range(
+            new vscode.Position(position.line, position.character - 1),
+            position
+          )
+        );
 
-          return completion;
-        };
-
-        return pipe(dict, R.keys, A.map(addCompletion));
+        return pipe(
+          dict,
+          Rec.keys,
+          A.filter((alias) => alias.includes(previousCharacter)),
+          A.map(addCompletion(importFormat))
+        );
       },
     }
   );
 
-  context.subscriptions.push(completion);
+  context.subscriptions.push(disposable);
 }
+
+const addCompletion = (importFormat: string) => (alias: keyof typeof dict) => {
+  const rootImportName = dict[alias];
+  const completion = new vscode.CompletionItem(alias);
+  completion.additionalTextEdits = [
+    new vscode.TextEdit(
+      new vscode.Range(new vscode.Position(0, 0), new vscode.Position(0, 0)),
+      importFormat === "module"
+        ? `import * as ${alias} from "fp-ts/${
+            lowercaseModuleNames.includes(rootImportName)
+              ? rootImportName
+              : capitalize(rootImportName)
+          }";\n`
+        : `import { ${rootImportName} as ${alias} } from "fp-ts";\n`
+    ),
+  ];
+  completion.kind = vscode.CompletionItemKind.Module;
+  completion.commitCharacters = ["."];
+  completion.insertText = alias;
+  completion.command = {
+    command: "editor.action.triggerSuggest",
+    title: "Re-trigger completions...",
+  };
+
+  return completion;
+};
 
 function capitalize(string: string) {
   return string[0].toUpperCase() + string.slice(1);
